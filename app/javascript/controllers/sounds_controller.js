e40e809b350bd07773cc6b73000ad2277b4ad403
Ervan Silvert <ervan@derniercri.io>
2023-02-02 18:25:31 +0100

    :construction: Adds more sounds

--- app/javascript/controllers/sounds_controller.js
+++ app/javascript/controllers/sounds_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["ready", "game-over", "tic"];
+  static targets = ["ready", "game-over", "tic", "error"];
 
   connect() {
     console.log("[Sounds] Connected");
