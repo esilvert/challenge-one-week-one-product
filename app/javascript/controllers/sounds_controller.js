8cbea7a7a8cff3f9122a208af10dff0e2d1e7f3f
Ervan Silvert <ervan@derniercri.io>
2023-02-02 18:15:11 +0100

    :sparkles: Adds audio sounds \o/ (tic here)

---
+++ app/javascript/controllers/sounds_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["ready", "game-over", "tic"];
+
+  connect() {
+    console.log("[Sounds] Connected");
+  }
+
+  play(event) {
+    if (this.targets.find(event.params.id)) {
+      console.log(`[Sounds] Playing sound id: ${event.params.id}`);
+
+      const sound = this[`${event.params.id}Target`];
+
+      sound.currentTime = 0;
+      sound.play();
+    } else console.log(`[Sounds] Unrocognized sound id: ${event.params.id}`);
+  }
+
+  disconnect() {}
+
+  private;
+}
