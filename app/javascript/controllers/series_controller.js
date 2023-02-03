6e575b27b2fac575300d0a3e3665b252ed8b6827
Ervan Silvert <ervan@derniercri.io>
2023-02-03 09:37:57 +0100

    :recycle: Changes the success sound & hard core mode

--- app/javascript/controllers/series_controller.js
+++ app/javascript/controllers/series_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  HARD_MODE_THRESHOLD = 3;
+  HARD_MODE_THRESHOLD = 30;
   ON_FIRE_THRESHOLD = 5;
 
   static targets = [""];
