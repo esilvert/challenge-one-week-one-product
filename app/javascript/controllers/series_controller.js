bbfe392e0d87bc250ca21d67e50e61ac90bca8d9
Ervan Silvert <ervan@derniercri.io>
2023-02-03 14:43:40 +0100

    :adhesive_bandage: Hard mode now trigger when it becomes hard

--- app/javascript/controllers/series_controller.js
+++ app/javascript/controllers/series_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  HARD_MODE_THRESHOLD = 30;
+  HARD_MODE_THRESHOLD = 40;
   ON_FIRE_THRESHOLD = 5;
 
   static targets = [""];
