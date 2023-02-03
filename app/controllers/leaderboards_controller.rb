ed0eab6816abcb4901b0a55dafac5ce45a63a955
Ervan Silvert <ervan@derniercri.io>
2023-02-03 14:03:10 +0100

    :sparkles: Adds player position if under position 100

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -5,5 +5,9 @@ class LeaderboardsController < ApplicationController
 
   def home
     @players = Player.order(score: :desc).limit(100)
+    @player = Player.find(session[:id])
+
+    @is_ranked = @players.select { |p| p.id == session[:id] }
+    @position = Player.order(score: :desc).pluck(:id).find_index(session[:id]) + 1 unless @is_ranked
   end
 end
