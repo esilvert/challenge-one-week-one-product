53ea69b4299f8ec33b8e9eea3e27f8a6e1374a4d
Ervan Silvert <ervan@derniercri.io>
2023-02-03 14:32:13 +0100

    :sparkles: Adds a way to play sound on connect (game over)

---
+++ app/javascript/controllers/play_sound_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    console.log("[PlaySound] Connected");
+
+    const soundId = this.element.dataset.soundId;
+
+    window.setTimeout(() => {
+      const soundsControllerElement = document.getElementById("sounds");
+
+      console.log({ elem: soundsControllerElement });
+      const soundsController =
+        this.application.getControllerForElementAndIdentifier(
+          soundsControllerElement,
+          "sounds"
+        );
+
+      soundsController.play({ params: { id: soundId } });
+    }, 100);
+  }
+
+  disconnect() {}
+
+  private;
+}
